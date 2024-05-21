997dfdc1f3e601fa1f0352529fe760d9f37d0ad3
Victor3294 <vblum26@gmail.com>
2024-05-20 22:04:18 -0300

    feat: 🎸 Resolução do exercicio 5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const calcularImpostoDeRendaRetido = require("./calculo_imposto_de_renda");
+const calcularInss = require("./calculo_inss");
 const calcularSalarioLiquido = require("./calculo_salario_liquido");
+const readline = require('readline');
 
-console.log(calcularSalarioLiquido(4664.69))
+const input = readline.createInterface(
+    process.stdin,
+    process.stdout
+)
+
+input.question("Nome do funcionario ? ", (nomeDigitado) => {
+    input.question("Cpf do funcionario ? ", (cpfDigitado) => {
+        input.question("Mes do pagamento (numerico) ? ", (mesDigitado) => {
+            input.question("Salario Bruto do funcionario ? ", (salarioBrutoDigitado) => {
+                console.log(`--- Folha de Pagamento --- 
+                \nNome: ${nomeDigitado} 
+                \nCPF: ${cpfDigitado} 
+                \nSalario Bruto: R$ ${salarioBrutoDigitado} 
+                \nINSS: R$ ${calcularInss(salarioBrutoDigitado)}
+                \nImposto de Renda: R$ ${calcularImpostoDeRendaRetido(salarioBrutoDigitado)}
+                \nSalarioLiquido: R$ ${calcularSalarioLiquido(salarioBrutoDigitado)}
+                `)
+                input.close()
+            })
+        })
+    })
+})
